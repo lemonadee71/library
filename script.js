1538955b744a0e08600cbc9415c4608f83f64906
Shin Andrei Riego <riegoshinandrei@gmail.com>
2020-11-24 19:22:48 +0800

    Added search results logic

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const GOOGLE_BOOKS = 'https://www.googleapis.com/books/v1/volumes?';
 const GOODREADS = 'https://www.goodreads.com/api/reviews_widget_iframe?did=75732&format=html'
 
 class Book {
+  static convertRatingToStars () {
+    if (this.rating || this.rating === "") {
+      return null;
+    }
+  
+    let decimal = +this.rating - Math.floor(+this.rating);
+    let stars = "\u2605".repeat(Math.floor(+this.rating));
+  
+    if (decimal <= 0.25 && decimal > 0) stars = stars + "\u00BC";
+    else if (decimal <= 0.5 && decimal > 0) stars = stars + "\u00BD";
+    else if (decimal <= 0.75 && decimal > 0) stars = stars + "\u00BE";
+    else if (decimal < 1 && decimal > 0) stars = stars + "\u2605";
+  
+    return stars;
+  };
+
   constructor(data) {
     this.id = data.id
     this.title = data.title
@@ -11,7 +27,7 @@ class Book {
     this.publishedDate = data.publishedDate
     this.publisher = data.publisher
     this.thumbnail = data.thumbnail
-    this.rating = data.rating
+    this.rating = Book.convertRatingToStars()
     this.isbn = data.isbn
     this.tags = [] 
   }
@@ -28,6 +44,53 @@ class Book {
 
   removeAllTags() {
     this.tags = []
+  }
+}
+
+class BookCardVariant extends Book {
+  constructor(data) {
+    super(data)
+    this.card = document.createElement('div')
+    this.tooltip = document.createElement('div')
+    this.elements = {
+      img: document.createElement('img'),
+      title: document.createElement('p'),
+      author: document.createElement('p'),
+      datePublished: document.createElement('span'),
+      rating: document.createElement('span'),
+      pageCount: document.createElement('span'),
+      desc: document.createElement('p'),
+      publisher: document.createElement('p'),
+      button: document.createElement('button'),
+    }
+  }
+
+  render(callback) {
+    // Add text
+    this.elements.img.src = this.thumbnail
+    this.elements.title.textContent = this.title
+    this.elements.author.textContent = this.author
+    this.elements.datePublished.textContent = this.publishedDate
+    this.elements.rating.textContent = this.rating
+    this.elements.pageCount.textContent = this.pageCount
+    this.elements.desc.textContent = this.description
+    this.elements.publisher.textContent = this.publisher
+    this.elements.button.textContent = 'Add Book'
+    this.elements.button.addEventListener('click', callback)
+
+    // Add classes
+    this.card.classList.add('tooltip')
+    this.tooltip.classList.add('tooltip-content')
+    this.tooltip.appendChild(this.elements.desc)
+
+    // Append elements
+    for (let name in this.elements) {
+      if (name !== 'desc')
+        this.card.appendChild(this.elements[name])
+    }    
+    this.card.appendChild(this.tooltip)
+
+    return this.card;
   }
 }
 
@@ -58,22 +121,6 @@ class BookCard extends Book {
       modalContent: document.createElement('div'),  
     }    
   }
-
-  static convertRatingToStars() {
-    if (!this.rating || this.rating === "") {
-      return null;
-    }
-  
-    let decimal = +this.rating - Math.floor(+this.rating);
-    let stars = "\u2605".repeat(Math.floor(+this.rating));
-  
-    if (decimal <= 0.25 && decimal > 0) stars = stars + "\u00BC";
-    else if (decimal <= 0.5 && decimal > 0) stars = stars + "\u00BD";
-    else if (decimal <= 0.75 && decimal > 0) stars = stars + "\u00BE";
-    else if (decimal < 1 && decimal > 0) stars = stars + "\u2605";
-  
-    return stars;
-  };
 
   addElement(face, element) {
     if (face === 'front')
@@ -112,7 +159,7 @@ class BookCard extends Book {
     this.backElements.author.textContent = this.author
     this.backElements.datePublished.textContent = this.publishedDate.slice(0, 4)
     this.backElements.pageCount.textContent = `${this.pageCount} pages`
-    this.backElements.rating.textContent = `${BookCard.convertRatingToStars()}`
+    this.backElements.rating.textContent = `${convertRatingToStars(this.rating)}`
     this.backElements.desc.textContent = this.description
     this.backElements.publisher.textContent = `Published by ${this.publisher}`
 
@@ -227,6 +274,13 @@ class Library extends List {
 }
 
 const App = (doc => {
+  let searchBtn = doc.getElementById('searchBook'),
+    searchModal = doc.getElementById('searchModal'),
+    searchForm = doc.getElementById('searchForm'),
+    searchResults = doc.getElementById('searchResults')
+
+  const library = new Library(doc.getElementById('library'))
+
   let params = (title, author) => {
     return {
       title,
@@ -259,13 +313,49 @@ const App = (doc => {
     return book;
   }
 
-  const getSearchResults = (params) => {
-    let endString = params.publisher ? `+inpublisher:${params.publisher}` : ''
+  async function getSearchResults(params) {
+    let publisher = params.publisher ? `+inpublisher:${params.publisher}` : ''
   
-    fetch(`${GOOGLE_BOOKS}q=${params.title}+inauthor:${params.author}${endString}`)
+    return fetch(`${GOOGLE_BOOKS}q=${params.title}+inauthor:${params.author}${publisher}`)
       .then(response => response.json())
       .then(results => results.items)
   }
 
+  const _showSearchModal = () => {
+    searchModal.style.display = 'block' 
+  }
 
+  const _hideSearchModal = (e) => {
+    if (e.target.id === 'searchModal')
+      searchModal.style.display = 'none'
+  }
+
+  const _renderResults = (results) => {
+    results.forEach(item => {
+      let result = new BookCardVariant(getBookData(item))
+      searchResults.appendChild(result.render())
+    })   
+  }
+
+  async function _searchBook(e) {
+    e.preventDefault()
+    
+    let title = doc.getElementById('search-title').value,
+      author = doc.getElementById('search-author').value
+
+    let results = await getSearchResults(params(title, author))
+    _renderResults(results)
+  }
+
+  const initialize = () => {
+    searchBtn.addEventListener('click', _showSearchModal)
+    searchModal.addEventListener('click', _hideSearchModal)
+    searchForm.addEventListener('submit', _searchBook)
+  }
+
+  return {
+    initialize,
+  }
 })(document)
+
+App.initialize()
